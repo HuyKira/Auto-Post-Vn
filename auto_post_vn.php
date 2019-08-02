5d85b9806bcb3ea5fede197e839978207279607a
Huy Kira <huyvq@bap.jp>
2019-08-02 09:57:32 +0700

    Huy Kira fix function get current URL

--- auto_post_vn.php
+++ auto_post_vn.php
@@ -372,20 +372,8 @@ function qnv_get_express( $link, $cat, $status ) {
 <?php }
 
 function gnv_curPageURL() {
-	$pageURL = 'http';
-	if ( ! empty( $_SERVER['HTTPS'] ) ) {
-		if ( $_SERVER['HTTPS'] == 'on' ) {
-			$pageURL .= "s";
-		}
-	}
-	$pageURL .= "://";
-	if ( $_SERVER["SERVER_PORT"] != "80" ) {
-		$pageURL .= $_SERVER["SERVER_NAME"] . ":" . $_SERVER["SERVER_PORT"] . $_SERVER["REQUEST_URI"];
-	} else {
-		$pageURL .= $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"];
-	}
-
-	return $pageURL;
+	$pageURL = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+    return $pageURL;
 }
 
 function gnv_get_meta_values( $key = '', $type = 'post' ) {
